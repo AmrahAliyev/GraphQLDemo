41be664eede3bdee8fe83680ced5dbff59c68288
AmrahAliyev <aliyevamrah0@gmail.com>
2024-08-07 08:40:47 +0400

    change

--- GraphQLDemo.Api/Schema/Queries/Query.cs
+++ GraphQLDemo.Api/Schema/Queries/Query.cs
@@ -23,7 +23,8 @@ namespace GraphQLDemo.Api.Schema.Queries
         {
             return await _userQuery.GetUser(id);
         }
-        [Description("Bütün istifadəçiləri gətirir")]
+
+        [GraphQLDescription("Bütün istifadəçiləri gətirir")]
         public async Task<IEnumerable<UserDto>> GetUsers()
         {
             return await _userQuery.GetUsers();
